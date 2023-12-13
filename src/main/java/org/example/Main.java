980365a1f2bf82947bd77f5af898c94376bb49d0
JulianSoe <j.soe@gmx.de>
2023-12-13 13:46:58 +0100

    jetzt gehts los

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int count =0;
         while (true) {
             Thread.sleep(2*1000);
-            System.out.println("Jetzt gehts richtig los");
+            System.out.println("Jetzt gehts erst richtig los");
             System.out.println(format("App running ... %d", count++));
         }
     }
