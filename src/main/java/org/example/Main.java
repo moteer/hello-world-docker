5c6fa9d1eddff550918c57b3ef8bae4bb157046b
[Grab I.] <grabigor1990@gmail.com>
2023-12-13 09:46:00 +0100

    changed main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,7 @@ import static java.lang.String.format;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
-        int count =0;
-        while (true) {
-            Thread.sleep(2*1000);
             System.out.println("Jetzt gehts los");
-            System.out.println(format("App running ... %d", count++));
+            System.out.println(format("App running ... "));
         }
     }
-}
