6db4d16285eedab590c92bab787cb3f004b79bef
JulianSoe <j.soe@gmx.de>
2023-12-13 14:08:00 +0100

    auf die Plätze

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int count =0;
         while (true) {
             Thread.sleep(2*1000);
-            System.out.println("Auf los gehts los");
+            System.out.println("Auf die Pläääätze...");
             System.out.println(format("App running ... %d", count++));
         }
     }
